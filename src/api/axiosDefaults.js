9febb6b3ea7605326be56ddb6b910f42b52d1dc7
Karol S <karol96266@gmail.com>
2023-12-09 14:05:52 +0000

    Add SignInForm, start with Context

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://momentos-pl-732efa6e2e31.herokuapp.com/';
+axios.defaults.baseURL = 'https://rest-framework-v2-e5f7a43eadcc.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
