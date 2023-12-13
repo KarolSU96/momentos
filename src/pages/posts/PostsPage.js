5e254de494d7e9c6c89d2510e458c12c5a209a11
Karol S <karol96266@gmail.com>
2023-12-13 13:31:56 +0000

    Add seachbar to search posts

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -11,7 +11,7 @@ import styles from "../../styles/PostsPage.module.css";
 import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
 
-import NoResults from '../../assets/no-results.png';
+import NoResults from "../../assets/no-results.png";
 import Asset from "../../components/Asset";
 
 function PostsPage({ message, filter = "" }) {
@@ -19,10 +19,12 @@ function PostsPage({ message, filter = "" }) {
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
 
+  const [query, setQuery] = useState("");
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
         setPosts(data);
         setHasLoaded(true);
       } catch (err) {
@@ -31,28 +33,49 @@ function PostsPage({ message, filter = "" }) {
     };
 
     setHasLoaded(false);
-    fetchPosts();
-  }, [filter, pathname]);
+    const timer = setTimeout(() => {
+      fetchPosts();
+    }, 1000)
+    return() =>{
+      clearTimeout(timer);
+    }
+  }, [filter, query, pathname]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          <Form.Control
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search posts"
+          />
+        </Form>
+
         {hasLoaded ? (
           <>
             {posts.results.length ? (
-              posts.results.map(post => (
-                <Post key={post.id} {...post} setPosts={setPosts}/>
+              posts.results.map((post) => (
+                <Post key={post.id} {...post} setPosts={setPosts} />
               ))
-            ) : <Container className={appStyles.Content}>
+            ) : (
+              <Container className={appStyles.Content}>
                 <Asset src={NoResults} message={message}></Asset>
-              </Container>}
+              </Container>
+            )}
           </>
         ) : (
           <Container className={appStyles.Content}>
-            <Asset spinner/>
+            <Asset spinner />
           </Container>
-        ) }
+        )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
         <p>Popular profiles for desktop</p>
