1a192907f0d8580fd4c6b0766a6d3f02f907995d
Karol S <karol96266@gmail.com>
2023-12-21 13:33:05 +0000

    Add the Profile Page, follow/ unfollow, edit profile functionality

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -14,11 +14,13 @@ import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 import { useSetCurrentUser } from "../../context/CurrentUserContext";
+import { useRedirect } from "../../hooks/useRedirect";
 
 
 function SignInForm() {
     //   Add your component logic here
     const setCurrentUser = useSetCurrentUser();
+    useRedirect('loggedIn')
     const [signInData, setSignInData] = useState({
         username: "",
         password: "",
@@ -40,7 +42,7 @@ function SignInForm() {
         try {
             const {data} = await axios.post("/dj-rest-auth/login/", signInData);
             setCurrentUser(data.user)
-            history.push("/");
+            history.goBack();
         } catch (err) {
             setErrors(err.response?.data);
         }
